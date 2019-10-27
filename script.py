da0e38520e9414ec9e0b56b8ebcbe9cb87d1861f
Richard Wood <richardwood@richards-mbp-3.home>
2019-10-27 17:19:32 +0000

    feat: returning first 5 items in ascending order sorted by current rent

--- script.py
+++ script.py
@@ -2,12 +2,21 @@ import csv
 
 # function that reads the csv file.
 # opens the csv file and converts it to the DictReader object and casts that to list. 
-# encapsulates all of the dictionaries into a list
+# encapsulates all of the dictionaries into a list.
 def get_csv_data():
     with open("Mobile_Phone_Masts_01.04.2019a.csv") as csv_file: 
         csv_reader = csv.DictReader(csv_file, delimiter=',')
         return list(csv_reader)
 
 
-csv_data = get_csv_data() 
-print(csv_data)
+# function that sorts through the data, sorted by the key using lambda in ascending order returning the first 5.
+def rent_costs_in_acsending_order(data):
+    result = sorted(data, key=lambda d: float(d['Current Rent']))
+    return result[:5]
+
+
+
+csv_data = get_csv_data()
+sorted_by_ascending = rent_costs_in_acsending_order(csv_data)
+print(sorted_by_ascending)
+
