eca31ea5e673f5e2c223b403ae94171944463a64
Richard Wood <richardwood@richards-mbp-3.home>
2019-10-27 18:24:00 +0000

    feat: function adding tenant names to list removing duplicates

--- script.py
+++ script.py
@@ -16,14 +16,21 @@ def rent_costs_in_acsending_order(data):
 
 
 # list comprehension - creating a new list from items, adding the item for every item in data  
-# if the number of Lease Years is equal to 25 append item to list 
+# if the number of Lease Years is equal to 25 append item to list. 
+# iterates and checks in one single expression
 def lease_of_twenty_five_years(data):
     return [item for item in data if item.get('Lease Years') == '25']
 
+
+# iterates over items and where tenant name create that as a key and count on how many masts
+# added the tenant name to the list and cast the list to a set which will remove any duplicates.
+def get_tenant_name_list(data):
+    return set([item['Tenant Name'] for item in data])
 
 
 csv_data = get_csv_data()
 sorted_by_ascending = rent_costs_in_acsending_order(csv_data)
 lease_years_of_twenty_five = lease_of_twenty_five_years(csv_data)
-print(lease_years_of_twenty_five)
+tenant_names = get_tenant_name_list(csv_data)
+print(tenant_names)
 
